a50b1bef962e4617bbd81e3ba03cd129d6bded50
Saikhnaa1206 <24HP1905@nest.edu.mn>
2024-08-20 15:13:17 +0800

    todo1

---
+++ index.js
@@ -0,0 +1,79 @@
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+const eye = document.getElementById("eye");
+const alphabet = "qwertyuiopasdfghjklzxcvbnm";
+const too = "0123456789";
+let num = 0;
+let valid = 0;
+function validEmail() {
+  const value = email.value;
+  if (value.includes("@")) {
+    // alert("email is correct");
+    valid++;
+  }
+  // else {
+  //   alert("email is wrong");
+  // }
+}
+function validPhone() {
+  const phoneNumber = number.value;
+  for (let i = 0; i < alphabet.length; i++) {
+    if (phoneNumber.includes(alphabet[i])) {
+      num++;
+    }
+  }
+  if (num == 0 && phoneNumber.length == 8) {
+    // alert("phone number is correct");
+    valid++;
+  }
+  // else {
+  //   alert("phone number is wrong");
+  // }
+}
+function validPass() {
+  const passwordValue = password.value;
+  let num1 = 0;
+  if (passwordValue.length >= 8) {
+    num1++;
+  }
+  for (let i = 0; i < alphabet.length; i++) {
+    if (passwordValue.includes(alphabet[i])) {
+      num1++;
+      break;
+    }
+  }
+  for (let j = 0; j < too.length; j++) {
+    if (passwordValue.includes(too[j])) {
+      num1++;
+      break;
+    }
+  }
+  if (num1 === 3) {
+    // alert("password is correct");
+    valid++;
+  }
+  // else {
+  //   alert("password is wrong");
+  // }
+}
+function workk() {
+  validEmail();
+  validPhone();
+  validPass();
+  if (valid == 3) {
+    window.location.href = "todo.html";
+  }
+}
+submit.onclick = workk;
+let numberPass = 0;
+function pass() {
+  if (numberPass % 2 == 0) {
+    password.type = "password";
+  } else {
+    password.type = "text";
+  }
+  numberPass++;
+}
+eye.onclick = pass;
---
+++ todo.js
@@ -0,0 +1,84 @@
+const input = document.getElementsByClassName("input")[0];
+const buttonAdd = document.getElementsByClassName("buttonAdd")[0];
+let todolist = document.getElementsByClassName("todolist")[0];
+let containerDone = document.createElement("div");
+containerDone.className = "container";
+document.body.appendChild(containerDone);
+containerDone.innerText = "Done";
+function create() {
+  if (input.value != "") {
+    const dateContainer = document.createElement("div");
+    const date = new Date();
+    const formatter = new Intl.DateTimeFormat("en-GB", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    const formattedTime = formatter.format(date);
+    dateContainer.innerHTML = formattedTime;
+    dateContainer.className = "dateContainer";
+    const editor = document.createElement("div");
+    editor.className = "editor";
+    const edit = document.createElement("button");
+    edit.className = "edit";
+    edit.innerHTML = "edit";
+    const dlt = document.createElement("button");
+    dlt.className = "dlt";
+    dlt.innerHTML = "delete";
+    const done = document.createElement("button");
+    done.className = "done";
+    done.innerHTML = "done";
+    const todo = document.createElement("div");
+    const text = document.createElement("div");
+    const styleDiv = document.createElement("div");
+    styleDiv.className = "stylee";
+    text.innerHTML = input.value;
+    text.className = "text";
+    todo.className = "todo";
+    editor.appendChild(edit);
+    editor.appendChild(dlt);
+    editor.appendChild(done);
+    styleDiv.appendChild(text);
+    styleDiv.appendChild(dateContainer);
+    todolist.appendChild(todo);
+    styleDiv.appendChild(editor);
+    todo.appendChild(styleDiv);
+    input.value = "";
+    dlt.addEventListener("click", function () {
+      todo.remove();
+    });
+    done.addEventListener("click", function () {
+      containerDone.appendChild(todo);
+      editor.remove();
+    });
+    edit.addEventListener("click", function () {
+      // const editedText = prompt("editlesen zuilee oruulna uu", text.innerText);
+      // text.innerText = editedText;
+      const roww = document.createElement("div");
+      edit.style.pointerEvents = "none";
+      const editInput = document.createElement("input");
+      roww.appendChild(editInput);
+      const doneButton = document.createElement("button");
+      doneButton.innerHTML = "done";
+      roww.appendChild(doneButton);
+      todo.appendChild(roww);
+      editInput.value = text.innerHTML;
+
+      doneButton.addEventListener("click", function () {
+        if (editInput.value == "") {
+          alert("Empty");
+        } else {
+          text.innerHTML = editInput.value;
+          editInput.style.display = "none";
+          doneButton.style.display = "none";
+          edit.style.pointerEvents = "auto";
+        }
+      });
+    });
+  }
+}
+
+buttonAdd.onclick = create;
